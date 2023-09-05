2bc9cb18e1fc40d0928fbbce960dd4c765e931c5
Jason J Pulikkottil <69060176+jasonjpulikkottil@users.noreply.github.com>
2023-09-05 12:49:35 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: ["Item 1", "Item 2"],
+      txtContent: "",
+    };
+  }
+
+  txtChange = (e) => {
+    this.setState({ txtContent: e.target.value });
+  };
+
+  addItem = (e) => {
+    let currentText = this.state.txtContent;
+    let currentItems = this.state.items;
+    currentItems.push(currentText);
+    this.setState({ items: currentItems });
+  };
+  removeItem = (i) => {  
+    if(!window.confirm("Are you sure you want to delete this?"))  {
+      return;
+    }
+    let currentItems = this.state.items;
+    currentItems.splice(i,1);
+    this.setState({ items: currentItems });
+  };
+
+  render() {
+    return (
+      <div className="">
+        <input type="text" onChange={this.txtChange} />
+        &nbsp;
+        <button onClick={this.addItem}>Add</button>
+        <ul>
+          {this.state.items.map((i, k) => (
+            <li>
+              {i} &nbsp;
+              <button onClick={()=>{this.removeItem(k)}}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
